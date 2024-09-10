e838f1f27491481a34288b2255adf4191ab7dfac
Robert Tolentino M. <robert.tolentino@terranovatrading.com.pe>
2024-09-10 17:35:55 -0500

    Send info to template report kardex

--- routes/utils/helpers.py
+++ routes/utils/helpers.py
@@ -2,6 +2,44 @@ import base64
 import pdfkit
 
 from flask import make_response
+from connection.database import execute_query
+
+
+def get_cabecera_formato(tabla: str, id_formato: str) -> list:
+    """
+    Función para obtener la cabecera de un formato dado.
+
+    :param tabla: Nombre de la tabla específica ('lavadosmanos', 'kardex', etc.)
+    :param id_formato: Identificador del formato (e.g., idlavadomano, idkardex)
+    :return: Resultado de la consulta de la cabecera
+    """
+    # Mapeo de tablas a sus respectivos IDs
+    id_column_map = {
+        'lavadosmanos': 'idlavadomano',
+        'kardex': 'idkardex'
+        # Agrega más mapeos según sea necesario para otros formatos
+    }
+
+    # Verificar si la tabla tiene un ID mapeado
+    if tabla not in id_column_map:
+        raise ValueError(
+            f"Tabla '{tabla}' no está configurada correctamente en el mapeo de IDs.")
+
+    # Construir la consulta SQL para la cabecera
+    # cabecera = execute_query(f"SELECT tf.idtipoformato, tf.nombreformato, tf.frecuencia, tf.codigo, lm.idlavadomano
+    #                            FROM tiposformatos tf
+    #                            INNER JOIN lavadosmanos lm ON tf.idtipoformato=lm.fk_idtipoformatos
+    #                            WHERE idlavadomano={formato_lavado_id}")
+    id_column = id_column_map[tabla]
+    query = f"""
+        SELECT tf.idtipoformato, tf.nombreformato, tf.frecuencia, tf.codigo, {tabla}.{id_column}
+        FROM tiposformatos tf
+        INNER JOIN {tabla} ON tf.idtipoformato = {tabla}.fk_idtipoformatos
+        WHERE {id_column} = %s
+    """
+
+    # Ejecutar la consulta y devolver el resultado
+    return execute_query(query, (id_formato,))
 
 
 def image_to_base64(image_path):
@@ -9,7 +47,7 @@ def image_to_base64(image_path):
         return base64.b64encode(image_file.read()).decode('utf-8')
 
 
-def generar_reporte(template, title_report='Reporte sin título', orientation='portrait'):
+def generar_reporte(template, filename_report='Reporte sin nombre', orientation='portrait'):
     """
     Genera un reporte en formato PDF a partir de una plantilla HTML.
 
@@ -38,12 +76,13 @@ def generar_reporte(template, title_report='Reporte sin título', orientation='p
             wkhtmltopdf="tools/wkhtmltox/bin/wkhtmltopdf.exe")
 
         # Generar el PDF en memoria desde la cadena HTML de la plantilla
-        pdf_content = pdfkit.from_string(template, False, configuration=config, options=options)
+        pdf_content = pdfkit.from_string(
+            template, False, configuration=config, options=options)
 
         # Crear la respuesta HTTP con el contenido PDF
         response = make_response(pdf_content)
         response.headers['Content-Type'] = 'application/pdf'
-        response.headers['Content-Disposition'] = f'attachment; filename={title_report}.pdf'
+        response.headers['Content-Disposition'] = f'attachment; filename={filename_report}.pdf'
         return response
     except Exception as e:
         print(f"Error al generar el reporte: {e}")
--- static/js/kardex.js
+++ static/js/kardex.js
@@ -404,18 +404,46 @@ window.onload = function() {
     }
 };
 
-function descargarFormatoKardex() {
+
+async function descargarFormatoKardex() {
     var idkardex = document.getElementById('idkardex_hidden').value;
-    $.get(`/kardex/descargar_formato_kardex/${idkardex}`, function(response) {
-        console.log(idkardex)
-    }).fail(function() {
-        Swal.fire({
-            icon: 'error',
-            title: 'Error',
-            text: 'Hubo un error al generar el reporte.',
-        });
-    });
-}
+    const endpoint = `/kardex/descargar_formato_kardex/${idkardex}`;
+
+    try {
+      const response = await fetch(endpoint, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        }
+      });
+
+      // Verificar si la respuesta es exitosa
+      if (!response.ok) {
+        throw new Error("Error al generar el reporte");
+      }
+
+      // Obtener el archivo blob de la respuesta
+      const blob = await response.blob();
+      // Obtener el nombre del archivo desde los headers
+      const contentDisposition = response.headers.get("Content-Disposition");
+      // Extraer el nombre del archivo si está presente en los headers
+      const fileNameMatch = contentDisposition && contentDisposition.match(/filename="?(.+)"?/);
+      const fileName = fileNameMatch ? fileNameMatch[1] : "reporte_kardex.pdf";
+
+      // Crear una URL para el blob y preparar la descarga
+      const url = window.URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = fileName;
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+      window.URL.revokeObjectURL(url); // Revocar la URL creada
+    } catch (error) {
+      console.error("Error generating report:", error);
+    }
+  }
+
 
 function finalizarKardex(idKardex) {
     $.post('/kardex/finalizar_kardex', {
