829d2638bad0b53e33d3892d65f98bfc0dd05e81
Robert Tolentino M. <robert.tolentino@terranovatrading.com.pe>
2024-09-17 12:02:18 -0500

    Add fetchDownloadPDF to js in main

--- static/js/kardex.js
+++ static/js/kardex.js
@@ -408,40 +408,7 @@ window.onload = function() {
 async function descargarFormatoKardex() {
     var idkardex = document.getElementById('idkardex_hidden').value;
     const endpoint = `/kardex/descargar_formato_kardex/${idkardex}`;
-
-    try {
-      const response = await fetch(endpoint, {
-        method: "GET",
-        headers: {
-          "Content-Type": "application/json",
-        }
-      });
-
-      // Verificar si la respuesta es exitosa
-      if (!response.ok) {
-        throw new Error("Error al generar el reporte");
-      }
-
-      // Obtener el archivo blob de la respuesta
-      const blob = await response.blob();
-      // Obtener el nombre del archivo desde los headers
-      const contentDisposition = response.headers.get("Content-Disposition");
-      // Extraer el nombre del archivo si está presente en los headers
-      const fileNameMatch = contentDisposition && contentDisposition.match(/filename="?(.+)"?/);
-      const fileName = fileNameMatch ? fileNameMatch[1] : "reporte_kardex.pdf";
-
-      // Crear una URL para el blob y preparar la descarga
-      const url = window.URL.createObjectURL(blob);
-      const a = document.createElement("a");
-      a.href = url;
-      a.download = fileName;
-      document.body.appendChild(a);
-      a.click();
-      a.remove();
-      window.URL.revokeObjectURL(url); // Revocar la URL creada
-    } catch (error) {
-      console.error("Error generating report:", error);
-    }
+    fetchDownloadPDF(endpoint, 'kardex');
   }
 
 
