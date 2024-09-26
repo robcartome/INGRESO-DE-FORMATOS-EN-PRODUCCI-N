dce86c9b45c3d53682b7f04d9918895441f85e31
RobertTM.MISTR <robert.tolentino@terranovatrading.com.pe>
2024-09-25 19:00:34 -0500

    Add data to reporte limpieza de equipos y medicion

--- routes/utils/helpers.py
+++ routes/utils/helpers.py
@@ -20,7 +20,8 @@ def get_cabecera_formato(tabla: str, id_formato: str) -> list:
         'condiciones_ambientales': 'idcondicionambiental',
         'registros_controles_envasados': 'id_registro_control_envasados',
         'controles_higiene_personal': 'id_control_higiene_personal',
-        'verificacion_limpieza_desinfeccion_areas': 'id_verificacion_limpieza_desinfeccion_area'
+        'verificacion_limpieza_desinfeccion_areas': 'id_verificacion_limpieza_desinfeccion_area',
+        'verificaciones_equipos_medicion': 'id_verificacion_equipo_medicion'
         # Agrega más mapeos según sea necesario para otros formatos
     }
 
